13da937b2569203280e1ed425fcdb7fc8facdbcb
CarlosLauter <toninhoyuki@gmail.com>
2026-03-09 00:29:43 -0300

    Terminando aula 05

---
+++ Carlos/JS/Aula 05/numerosAlgunsCuidados.js
@@ -0,0 +1,7 @@
+console.log(7 / 0) //infinity
+console.log("10" / 2) //viado que macumba
+console.log("Show!" * 2)//Not a Number = NaN
+console.log(0.1 + 0.7)
+//console.log(10.toString()) não da certo, mas se puser entre () dá
+console.log((10.345).toFixed(2))
+console.log((10).toString())

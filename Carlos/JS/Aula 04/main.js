8988d20a2e677009d68726a6c759cfae373a0975
CarlosLauter <toninhoyuki@gmail.com>
2026-03-09 00:19:24 -0300

    Terminando aula 04

---
+++ Carlos/JS/Aula 04/main.js
@@ -0,0 +1,13 @@
+let qualquer = 'Legal'
+console.log(qualquer)
+console.log(typeof qualquer)
+
+qualquer = 3.141516
+console.log(qualquer)
+console.log(typeof qualquer)
+
+//Que loucura, js é uma linguagem typada, dá pra mudar sabaça do nada
+//Evitar nome genéricos e sigas
+let valor = ''
+let numero = 1
+let pqp = false //Produto Químico Perigoso... kkk

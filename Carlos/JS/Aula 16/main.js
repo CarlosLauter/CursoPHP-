ebc82c722d1145d0c0779c5637c8132592353f51
CarlosLauter <toninhoyuki@gmail.com>
2026-03-11 21:56:07 -0300

    Finalizando aula 16

---
+++ Carlos/JS/Aula 16/main.js
@@ -0,0 +1,10 @@
+var numero = 1
+
+{
+    var numero = 2
+    console.log('dentro =', numero)
+}
+
+console.log('fora =', numero)
+
+//elas se sobrescreveram por serem variaveis dentro do escopo global

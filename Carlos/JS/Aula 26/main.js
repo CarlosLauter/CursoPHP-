15586d9988d8e3e95a6297e57b4259296dc113a3
CarlosLauter <toninhoyuki@gmail.com>
2026-03-11 23:06:41 -0300

    terminando aula 26

---
+++ Carlos/JS/Aula 26/main.js
@@ -0,0 +1,22 @@
+const pessoa = {
+    nome: 'Ana',
+    idade: 17,
+    endereco: {
+        logradouro: 'Rua Cuscuz',
+        numero: 257
+    }
+}
+
+const {nome, idade} = pessoa
+console.log(nome, idade)
+console.log(pessoa)
+
+const {nome: n, idade: i} = pessoa
+console.log(n, i)
+
+const {sobrenome, bemHumorada = true} = pessoa
+console.log(sobrenome, bemHumorada)
+
+const {endereco: {logradouro, numero, cep}} = pessoa
+console.log(logradouro, numero, cep)
+

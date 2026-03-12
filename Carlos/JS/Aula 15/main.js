d4486938b7b92d857e207590e4bebbbf49ce3cdd
CarlosLauter <toninhoyuki@gmail.com>
2026-03-11 21:53:03 -0300

    Terminando aula 15

---
+++ Carlos/JS/Aula 15/main.js
@@ -0,0 +1,18 @@
+{
+    {
+        {
+            {
+                var sera = 'sera??' //possível ver por não estar numa function
+            }
+        }
+    }
+}
+
+console.log(sera)
+
+function teste() {
+    var local = 123 //não vai dar pra ver por estar dentro de um escopo de function
+}
+// console.log(local)
+
+teste()
